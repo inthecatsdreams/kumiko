8c352cd77362f79f54b9902713f82d9c7101093c
Vitali Sokolov <68013505+boozerboozeman@users.noreply.github.com>
2021-07-15 16:29:49 +0100

    removed the music cog

--- kumiko.py
+++ kumiko.py
@@ -111,62 +111,6 @@ class General(commands.Cog):
         
         await ctx.send(embed=embed)
 
-    @commands.command()
-    async def clean_cache(self, ctx):
-        """Clear the music cache"""
-
-        files = glob.glob("*.webm")
-        if (len(files) > 0):
-            await ctx.send("Found {} files.".format(len(files)))
-            for i in files:
-                os.remove(i)
-            await ctx.send("Cache cleared.")
-        else:
-            await ctx.send("There is nothing to delete.")
-
-
-class Music(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def music(self, ctx, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(player, after=lambda e: print(
-                'Player error: %s' % e) if e else None)
-
-        await ctx.send('Now playing: {}'.format(player.title))
-
-    @commands.command()
-    async def volume(self, ctx, volume: int):
-        """Changes the player's volume"""
-
-        if ctx.voice_client is None:
-            return await ctx.send("Not connected to a voice channel.")
-
-        ctx.voice_client.source.volume = volume / 100
-        await ctx.send("Changed volume to {}%".format(volume))
-
-    @commands.command()
-    async def stop(self, ctx):
-        """Stops and disconnects the bot from voice"""
-
-        await ctx.voice_client.disconnect()
-
-    @yt.before_invoke
-    async def ensure_voice(self, ctx):
-        if ctx.voice_client is None:
-            if ctx.author.voice:
-                await ctx.author.voice.channel.connect()
-            else:
-                await ctx.send("You are not connected to a voice channel.")
-                raise commands.CommandError(
-                    "Author not connected to a voice channel.")
-        elif ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
 
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]),
@@ -181,6 +125,5 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.do_not_disturb, activity=game)
 
 
-bot.add_cog(Music(bot))
 bot.add_cog(General(bot))
 bot.run(config["token"])
